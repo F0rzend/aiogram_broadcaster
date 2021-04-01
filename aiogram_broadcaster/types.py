ae5a9ee6a7aad26b0a153a3a60154ed3ebb5c6e5
F0rzend <kostyajog@mail.ru>
2021-04-01 13:54:49 +0300

    0.0.5: typehints fix
    https://github.com/fonco/aiogram_broadcaster/issues/10

--- aiogram_broadcaster/types.py
+++ aiogram_broadcaster/types.py
@@ -5,8 +5,7 @@ from aiogram.types import (ForceReply, InlineKeyboardMarkup,
                            ReplyKeyboardMarkup, ReplyKeyboardRemove)
 
 ChatIdType = Union[int, str]
-ChatIdsType = Union[List[ChatIdType], ChatIdType]
-ChatsType = Union[ChatIdsType, List[Dict]]
+ChatsType = Union[Union[List[ChatIdType], ChatIdType], List[Dict]]
 TextType = Union[Template, str]
 MarkupType = Union[
     InlineKeyboardMarkup,
