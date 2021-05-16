1984694f4848dd4c0ef6a54a738bbae3a0e5cdbe
F0rzend <zhogalkostya@gmail.com>
2021-05-16 12:36:25 +0300

    Update Running Error

--- aiogram_broadcaster/exceptions.py
+++ aiogram_broadcaster/exceptions.py
@@ -1,7 +1,8 @@
-class RunningError(ValueError):
+class RunningError(RuntimeError):
     def __init__(self, is_running: bool = False):
         if not is_running:
             message = 'The broadcast is not running'
         else:
             message = 'The broadcast is already running'
+
         super().__init__(message)
